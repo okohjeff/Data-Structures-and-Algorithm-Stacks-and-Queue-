931a00c7e494022d4d997544d9d138bb9889a5ab
okohjeff <132713016+okohjeff@users.noreply.github.com>
2024-06-24 23:19:22 +0100

    Add files via upload

---
+++ Electronic.java
@@ -0,0 +1,14 @@
+public interface Electronic {
+
+    boolean onOrOff = false;
+
+    boolean on();
+
+    boolean off();
+
+    default void electronic(){
+        if (onOrOff){
+            System.out.println("Default");
+        }
+    }
+}
